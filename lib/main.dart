8df426237f51a94cd040a488e741f16d1ded3fb8
Zainab Alawami <z.m.a2017@gmail.com>
2023-04-01 22:26:45 +0300

    v2

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:hope_application/view/screen/ManageAccount.dart';
 import 'package:hope_application/view/screen/ManageSymptomsTraker2.dart';
 import 'package:hope_application/view/screen/RecoveryExercise.dart';
 import 'package:hope_application/view/screen/RecoveryExerciseModification.dart';
 import 'package:hope_application/view/screen/ManageSymptomsTracker1.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
